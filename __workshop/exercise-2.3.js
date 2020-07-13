51e1183feb46a389f6e56680c205a991c92d4c37
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-07-13 15:27:25 -0400

    exercise 2

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -5,3 +5,18 @@
 const request = require('request-promise');
 
 // get the code you wrote in 2.2 and paste it here...
+
+const greeting = (langCode) => {
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return parsedResponse; // 2
+    })
+    .catch((err) => err.error ? JSON.parse(err.error) : err);
+};
+
+// Testing
+greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+greeting('es').then((result) => console.log(result));
+greeting('ru').then((result) => console.log(result));
+greeting('test').then((result) => console.log(result));
