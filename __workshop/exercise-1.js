435f71bb10778453e5d2319f6fee3dd37a860363
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-07-13 13:47:36 -0400

    exercise 1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -4,11 +4,17 @@ const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  return new Promise((resolve, reject) => {
+    array.every(i => typeof i === 'string')
+      ? resolve(array.map(x => { return x.toUpperCase() }))
+      : reject('Error');
+  });
 };
 
 const sortWords = (array) => {
-  // write some code
+  return new Promise((resolve, reject) => {
+    resolve(array.sort());
+  })
 };
 
 // Calling (testing)
